298424628e5c3fa6909c0a8bdab51fd5fc2a0edd
054.kr <126878901+JuhoCha@users.noreply.github.com>
2024-09-02 09:02:51 +0900

    Create 수 비교(수정필).c

---
+++ 코스/2/수 비교(수정필).c
@@ -0,0 +1,54 @@
+#include <stdio.h>
+
+int main() {
+  int num1, num2, num3;   // 첫 번째 숫자, 두 번째 숫자, 세 번째 숫자 
+  int max_num, min_num;   // 가장 큰 숫자, 가장 작은 숫자
+
+    scanf("%d %d %d", &num1, &num2, &num3);
+    
+    if(num1>num2&&num2>num3){
+        max_num = num1;
+        min_num = num3;
+        printf("가장 큰 수 %d\n", max_num);
+        printf("가장 작은 수 %d\n", min_num);
+    }
+    else if(num1>num3&&num3>num2){
+        max_num = num1;
+        min_num = num2;
+        printf("가장 큰 수 %d\n", max_num);
+        printf("가장 작은 수 %d\n", min_num);
+    }
+    else if(num2>num1&&num1>num3){
+        max_num = num2;
+        min_num = num3;
+        printf("가장 큰 수 %d\n", max_num);
+        printf("가장 작은 수 %d\n", min_num);
+    }
+    else if(num2>num3&&num3>num1){
+        max_num = num2;
+        min_num = num1;
+        printf("가장 큰 수 %d\n", max_num);
+        printf("가장 작은 수 %d\n", min_num);
+    }
+    else if(num3>num1&&num1>num2){
+        max_num = num3;
+        min_num = num2;
+        printf("가장 큰 수 %d\n", max_num);
+        printf("가장 작은 수 %d\n", min_num);
+    }
+    else if(num3>num2&&num2>num1){
+        max_num = num3;
+        min_num = num1;
+        printf("가장 큰 수 %d\n", max_num);
+        printf("가장 작은 수 %d\n", min_num);
+    }
+    else if(num1=num2=num3){ //000이 안되는 문제 존재
+        min_num = num3;
+        max_num = num1;
+        printf("가장 큰 수 %d\n", max_num);
+        printf("가장 작은 수 %d\n", min_num);
+    }
+
+
+  return 0;
+}
