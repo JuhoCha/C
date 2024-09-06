2bfb91336be6dc528ba179aa95f621cd6b2e7713
054.kr <126878901+JuhoCha@users.noreply.github.com>
2024-09-06 21:17:38 +0900

    Update 2-1.c

--- C프/2-1.c
+++ C프/2-1.c
@@ -1,1 +1,25 @@
+#include <stdio.h>
+
+ int main() {
+     //정수 세 개 입력받기
+     //정수 입력받은거 if랑 모듈러 써서 배수 확인
+
+    int n1, n2, n3;
+    int count = 0;
+
+    scanf("%d %d %d", &n1, &n2, &n3);
+    if(n1%6==0){
+        count++;
+    }
+    if(n2%6==0){
+        count++;
+    }
+    if(n3%6==0){
+        count++;
+    }
+
+    printf("%d", count);
+
+     return 0;
+ }
 
