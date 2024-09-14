deb10dd98d98a76959accecfbb78ebf5f1e3c993
054.kr <126878901+JuhoCha@users.noreply.github.com>
2024-09-14 20:49:03 +0900

    Update 생각한 숫자 맞추기.c

--- 코스/4/생각한 숫자 맞추기.c
+++ 코스/4/생각한 숫자 맞추기.c
@@ -7,20 +7,22 @@ int main(){
     //맞추었다면 0을 입력한다.
     //1,2 이외의 숫자를 입력하면 반복을 종료
     //시도 횟수 출력
-    printf("How about 50? (try more : 1, try less : 2, right : 0)");
+    
     int result;
     int num;
-    int count;
-    
+    int count=0;
+    int i;
+    printf("How about 50? (try more : 1, try less : 2, right : 0)");
+    scanf("%d", &num);
     if(num == 1){
-        for(i = 50; i<=100; i++);
-        printf("How about %d? (try more : 1, try less : 2, right : 0)"\n);
-        count++;
+        for(i = 50; i<=100; i++);{
+        printf("How about %d? (try more : 1, try less : 2, right : 0)\n", i);
+        count++;}
     }
     if(num == 2){
-        for(i = 50; i>=0; i--);
-        printf("How about %d? (try more : 1, try less : 2, right : 0)"\n);
-        count++;
+        for(i = 50; i>=0; i--);{
+        printf("How about %d? (try more : 1, try less : 2, right : 0)\n", i);
+        count++;}
     }
     else{
         printf("%d tried.\n", count);
