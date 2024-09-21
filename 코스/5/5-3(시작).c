fda4f0fc22c9451460ffbf60ec90d0f809bd3dac
054.kr <126878901+JuhoCha@users.noreply.github.com>
2024-09-22 00:56:20 +0900

    Create 5-3(시작).c

---
+++ 코스/5/5-3(시작).c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main(){
+  float score[10];  			// 심사점수
+  float max=0.0, min=10.0; 				// 가장 큰 점수, 가장 작은 점수
+  float total=0.0; 				// 점수 총 합계
+  float average=0.0;  				// 평균점수
+  int i;  					// 반복문을 위한 변수
+  
+  //큰수랑 작은수일때 입력받은 수랑 비교함
+  //큰수는 큰수보다 크면으로 작은수는 작은수보다 작은수로 비교하고
+  //조건문써서 min max해당 안하는 애들만 모아서 평균
+  
+  scanf("%d", &score[10]);
+  for()
+
+
+  return 0;
+}
