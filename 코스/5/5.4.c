b19f34e4ca85b4a21f355f9ce40f019af9c02157
054.kr <126878901+JuhoCha@users.noreply.github.com>
2024-09-24 23:36:41 +0900

    Update and rename 5.4(중간).c to 5.4.c

--- 코스/5/5.4.c
+++ 코스/5/5.4.c
@@ -1,27 +1,29 @@
 #include <stdio.h>
 int main(){
    int num[10]; 				// 10개의 숫자
-   int first=0;  			    // 첫 번째로 큰 수
-   int second=0; 				// 두 번째로 큰 수
+   int first;  			    // 첫 번째로 큰 수
+   int second; 				// 두 번째로 큰 수
    int second_max_index=0; 		// 두 번째로 큰 수의 인덱스 
    int i;  					    // 반복문을 위한 변수
 
 //max보다 큰 수 하고
 //그 max보다 작은 수인데 &&해서 second 보다 큰 수 해서 
+//첫 번째 수보다 큰 수 나오면 그 수를 second로 만들고 그 i 저장?
 //두 번째 수 index로 알아내기..?
     for(i=0; i<10; i++){
         scanf("%d", &num[i]);
         
         if(num[i]>first){
+            second = first; 
             first = num[i];
+            second_max_index = i;
         }
-        if(first<num[i] && num[i]>second){
+        if(num[i]>second && first<num[i]){
             second = num[i];
             second_max_index = i;
         }
     }
     printf("Second largest number: %d (%dth)\n", second, second_max_index);
-    printf("%d", first);
 
   return 0;
 }
