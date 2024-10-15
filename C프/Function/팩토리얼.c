a2467ed88f471157603809f78af2720a4e276da6
054.kr <126878901+JuhoCha@users.noreply.github.com>
2024-10-16 01:43:04 +0900

    Create 팩토리얼.c

---
+++ C프/Function/팩토리얼.c
@@ -0,0 +1,22 @@
+// Online C compiler to run C program online
+#include <stdio.h>
+
+int factorial(int n){
+     if(n<=1){
+         return 1;
+     }else{
+         return n*factorial(n-1);
+     }
+ }
+ 
+int main() {
+    int n;
+    
+    scanf("%d", &n);
+    
+    printf("%d", factorial(n));
+    
+    return 0;
+}
+
+ 
