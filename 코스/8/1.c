bdbc71e2a56c2720125707f02b6bdd4dc0041ff5
054.kr <126878901+JuhoCha@users.noreply.github.com>
2024-10-15 17:12:53 +0900

    Create 1.c

---
+++ 코스/8/1.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+//사람 수 입력 --> scanf해서 사람 수 입력하고 카운트
+//그 수만큼 년도 입력하기 --> 년도 입력하고 현재년도에서 빼서 나이 구하고 
+//구간 나눠서 구간 구분하고 그만큼 카운트++
+int ageToGroup(int b);
+
+int count_person(int num){
+    scanf("%d", num);
+    return count_person(num);
+}
+
+int birth_year(int i, int num, int year[100]){
+    for(i=0; i<num; i++){
+        scanf("%d", int year[i])
+    }
+}
+
+int main() {
+    int birth_year(int i, int num, int year[100]); 			// 입력받은 태어난 년도
+    int year[100]; 			// 각 사람들의 생년 (최대 100명) 내가 입력한 년도
+    int count_person(int num); 		// 입력된 인원 수 입력한 수 
+    int count[6]; 			// 6가지 연령대별 숫자 각 연령층에 몇명인지 말함.
+    
+    return 0;
+}
+
