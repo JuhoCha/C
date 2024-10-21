466fe6bd738dfe145916037602f4352bcfeb15bd
054.kr <126878901+JuhoCha@users.noreply.github.com>
2024-10-22 00:53:43 +0900

    Update 7-5.c

--- 코스/7/7-5.c
+++ 코스/7/7-5.c
@@ -2,7 +2,6 @@
 #include <stdlib.h>
 #include <time.h>
 
-
 int main() {
     int coins;      // 사용자의 코인 수. (최초에 입력받음)
     int number[3];  // 랜덤하게 만들어진 슬롯머신의 숫자 3개 
@@ -14,21 +13,24 @@ int main() {
     scanf("%d", &coins);
     srand(time(0));
     
-    while(1){
-        //시작
+    while(1) {
+        // 시작
         count++;
         coins--;
-        printf("game %d회 Start!!! (아무 숫자나 입력하세요!)", count);
+        printf("game %d회 Start!!! (아무 숫자나 입력하세요!) ", count);
         scanf("%d", &dummy);
-        //난수 생성
-        if(dummy){
-            for(i=0; i<3; i++){
-                number[i]=rand()%9+1;
+        
+        // 난수 생성
+        if (dummy) {
+            for (i = 0; i < 3; i++) {
+                number[i] = rand() % 9 + 1;  // 1부터 9까지 숫자 생성
             }
-        }printf("%d %d %d ", number[i]);
+        }
+        
+        // 세 개의 숫자 출력
+        printf("%d %d %d\n", number[0], number[1], number[2]);
+        
         // 두 개 숫자 일치 확인
-        // 근데 숫자가 두개는 맞고 두 개 는 달라야 하니까
-        // 하나는 == 하나는 !=
         if ((number[0] == number[1] && number[0] != number[2]) || 
             (number[0] == number[2] && number[0] != number[1]) || 
             (number[1] == number[2] && number[0] != number[1])) {
@@ -44,16 +46,15 @@ int main() {
         else {
             printf("----> 꽝입니다.. 아쉽네요\n");
         }
+        
         printf("남아있는 코인은 %d개 입니다.\n", coins);
         
-        
-        // 앵꼬나면 끝
-        if(coins==0){
-            printf("게임 종료!!");
+        // 코인이 0이 되면 게임 종료
+        if (coins == 0) {
+            printf("게임 종료!!\n");
             break;
         }
     }
 
-    
     return 0;
 }
