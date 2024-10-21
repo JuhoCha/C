96039952227cbbc302e9c172528b0a6c189057a7
054.kr <126878901+JuhoCha@users.noreply.github.com>
2024-10-22 00:20:09 +0900

    Update 7.c

--- 코스/8/7.c
+++ 코스/8/7.c
@@ -8,11 +8,11 @@ int main() {
   int i;
   int max, min;
   
-  maxOfTen(number);
-  printf("%d", max);
+  max = maxOfTen(number);
+  printf("Maximum value: %d\n", max);
   
-  minOfTen(number); 
-  printf("%d", min);
+  min = minOfTen(number);
+  printf("Minimum value: %d\n", min);
 
 
   return 0;
@@ -21,16 +21,15 @@ int main() {
 int maxOfTen(int number[10]){
     int max = 0;
     for(int i=0; i<10; i++){
-        scanf("%d", number[i]);
+        scanf("%d", &number[i]);
         if(number[i]>max){
             max = number[i];
         }
     }return max;
 }
-int minOfTen(int number[10]){
+int minOfTen(int number[10]){ //같은 num받을때는 하나만 써줘도 된다
     int min = 100;
     for(int i=0; i<10; i++){
-        scanf("%d", number[i]);
         if(number[i]<min){
             min = number[i];
         }
