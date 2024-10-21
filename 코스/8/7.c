59320d3fe16820f4d789fcc3c345c63c29366056
054.kr <126878901+JuhoCha@users.noreply.github.com>
2024-10-21 19:00:29 +0900

    Create 7.c

---
+++ 코스/8/7.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+int maxOfTen(int number[10]);
+int minOfTen(int number[10]); 
+
+int main() {
+  int number[10] = {0};
+  int i;
+  int max, min;
+  
+  maxOfTen(number);
+  printf("%d", max);
+  
+  minOfTen(number); 
+  printf("%d", min);
+
+
+  return 0;
+}
+
+int maxOfTen(int number[10]){
+    int max = 0;
+    for(int i=0; i<10; i++){
+        scanf("%d", number[i]);
+        if(number[i]>max){
+            max = number[i];
+        }
+    }return max;
+}
+int minOfTen(int number[10]){
+    int min = 100;
+    for(int i=0; i<10; i++){
+        scanf("%d", number[i]);
+        if(number[i]<min){
+            min = number[i];
+        }
+    }return min;
+}
