de7c87530d1a29b9bb08d623c8e8535cb25b26e3
054.kr <126878901+JuhoCha@users.noreply.github.com>
2024-11-10 17:48:06 +0900

    Create 10-7.c

---
+++ 코스/10/10-7.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+//void info(char userid[10][20], char userpw[10][20]);
+
+int main() {
+  char userid[10][20];
+  char userpw[10][20];  
+
+FILE *file = fopen("user.txt", "r");
+for(int i=0; i<10; i++){
+    fscanf(file, "%s %s", userid[i], userpw[i]);
+}
+printf("List of registered users\n");
+printf("No ID   Password\n");
+for(int i=0; i<10; i++){
+    printf("%d %s %s\n", i+1, userid[i], userpw[i]);
+}
+
+fclose(file);
+  return 0;
+}
