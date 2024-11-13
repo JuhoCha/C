7b90d56979283f2bc98098d410ddfca9610b82e8
054.kr <126878901+JuhoCha@users.noreply.github.com>
2024-11-14 01:20:24 +0900

    Update 11-4.c

--- 코스/11/11-4.c
+++ 코스/11/11-4.c
@@ -9,22 +9,22 @@ struct point{
 float calcDistance(struct point p1, struct point p2); // 두 점의 거리 구하는 함수 
 //지금 p1과 p2에 x, y값이 들어가 있는거임.
 int main() {
-    float p1->x; 
-    float p1->y;
-    float p2->x;
-    float p2->y;
+    struct point p1, p2;
     
-    calcDistance(p1, p2);
     
-    scanf("%f %f", &p1.x, &p2.x);
-    scanf("%f %f", &p1.y, &p2.y);
+    scanf("%f %f", &p1.x, &p1.y);
+    scanf("%f %f", &p2.x, &p2.y);
+    
+    float distance = calcDistance(p1, p2);
+    
+    printf("%.1f", distance);
     
   return 0;
 }
 
 
 float calcDistance(struct point p1, struct point p2){
-    float *result = sqrt(pow((p1.x-p2.x), 2) + pow((p1.y-p2.y), 2));
+    float result = sqrt(pow((p1.x-p2.x), 2) + pow((p1.y-p2.y), 2));
     
     return result;
 }
